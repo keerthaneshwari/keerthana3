b9e0bdb58b44c662ec868ea5645150123657b88a
keerthaneshwari <keerthana07995@gmail.com>
2016-10-21 09:52:20 +0530

    Create pg3.cpp

---
+++ pg3.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a;
+ cout<<"\nenter the number";
+ cin>>a;
+ if(a>0)
+ { 
+  cout<<"\npositive";
+ }
+  else if(a<0)
+  {
+   cout<<"\nnegtive";
+  }
+  else
+  {
+   cout<<"\nzero";
+   }
+   return 0;
+   getch();
+   }
+   
+   
+ 
